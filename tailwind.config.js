85ac57d6b8835793b8798dfc60595db6218fd293
bibhalgithub <2206083@kiit.ac.in>
2025-06-21 00:00:12 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,39 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        purple: {
+          50: '#faf5ff',
+          100: '#f3e8ff',
+          200: '#e9d5ff',
+          300: '#d8b4fe',
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7e22ce',
+          800: '#6b21a8',
+          900: '#581c87',
+          950: '#3b0764',
+        },
+        green: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+          950: '#052e16',
+        },
+      },
       animation: {
         'slide-up': 'slideUp 0.3s ease-out',
-        'fade-in': 'fadeIn 0.3s ease-out',
+        'fade-in': 'fadeIn 1s ease-out',
+        'slide-in-left': 'slideInLeft 1s ease-out',
+        'slide-in-up': 'slideInUp 1s ease-out',
       },
       keyframes: {
         slideUp: {
@@ -13,8 +43,28 @@ export default {
           '100%': { transform: 'translateY(0)', opacity: 1 },
         },
         fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideInLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        slideInUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
         },
       },
     },
